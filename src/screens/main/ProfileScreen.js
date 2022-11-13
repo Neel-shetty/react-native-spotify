3e08a1f01650d0d5280923a50c39b1e7152331f5
Neel <neelnarayanshetty@gmail.com>
2022-11-13 16:08:01 +0530

    working build

--- src/screens/main/ProfileScreen.js
+++ src/screens/main/ProfileScreen.js
@@ -1,10 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Button from '../../components/ui/Button'
 
 const ProfileScreen = () => {
   return (
-    <View>
-      <Text>ProfileScreen</Text>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Button>Log Out</Button>
     </View>
   )
 }
