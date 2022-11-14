dbc01865b1d16155fe0cefcd5a19d5e532b18cd7
Neel <neelnarayanshetty@gmail.com>
2022-11-14 23:48:44 +0530

    redux

--- src/redux/features/songSlice/songSlice.js
+++ src/redux/features/songSlice/songSlice.js
@@ -1,6 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 import PlaylistData from "../../../../assets/dummydata/PlaylistData";
 import previewData from "../../../../assets/dummydata/previewData";
+import { Audio } from "expo-av";
+import { Sound } from "expo-av/build/Audio";
 
 const initialState = {
   songDetails: songStatus,
@@ -10,8 +12,12 @@ const songSlice = createSlice({
   name: "songDetails",
   initialState,
   reducers: {
-    setIsPlayingFalse: (state, action)=>{
-      state.isPlaying = true
-    }
-  }
+    // setIsPlayingFalse: (state, action)=>{
+    //   state.isPlaying = true
+    // }
+    pause: (state) => {
+      Sound.createAsync({shouldPlay: false})
+      state.songDetails.isPlaying = false
+    },
+  },
 });
